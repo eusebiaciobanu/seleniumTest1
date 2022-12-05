df4ce10d70484673c30c6cacbbdce5f64e12e253
sebastianciocan <seby.ciocan@gmail.com>
2022-12-05 20:45:42 +0200

    junit and css selectors

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,3 +1,4 @@
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -5,12 +6,38 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 
-    public void loginWithValidData(){
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    private WebDriver driver;
+
+    @Before
+    public void initDriver(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new ChromeDriver();
 
         driver.manage().window().maximize();
         driver.get("http://testfasttrackit.info/selenium-test/");
+    }
+
+    @Test
+    public void loginWithValidData(){
+        driver.findElement(By.cssSelector(".skip-account .label")).click();
+        driver.findElement(By.cssSelector("[title='Log In']")).click();
+        driver.findElement(By.id("email")).sendKeys("cosmin@fasttrackit.org");
+        driver.findElement(By.id("pass")).sendKeys("123456");
+        driver.findElement(By.id("send2")).click();
+
+        WebElement welcomeTextElement = driver.findElement(By.cssSelector(".hello strong"));
+
+        String expectedText = "Hello, Cosmin Fast!";
+        String actualText = welcomeTextElement.getText();
+
+        Assert.assertEquals(expectedText,actualText);
+
+        WebElement myAccount = driver.findElement(By.cssSelector(".block-account span"));
+
+        Assert.assertTrue(myAccount.isDisplayed());
+    }
+    @Test
+    public void loginWithValidData1(){
 
         driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
         driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
@@ -28,6 +55,32 @@ public class LoginTest {
         }else
             System.err.println("Nu s-a logat. ");
 
+    }
+
+    @Test
+    public void loginWithValidData2(){
+
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+        driver.findElement(By.id("email")).sendKeys("cosmin@fasttrackit.org");
+        driver.findElement(By.id("pass")).sendKeys("123456");
+        driver.findElement(By.id("send2")).click();
+
+        WebElement welcomeTextElement = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col2-left-layout > div > div.col-main > div.my-account > div > div.welcome-msg > p.hello > strong"));
+
+        String expectedText = "Hello, Cosmin Fast!";
+        String actualText = welcomeTextElement.getText();
+
+        if (actualText.equals(expectedText)){
+            System.out.println("S-a logat cu success!");
+        }else
+            System.err.println("Nu s-a logat. ");
+
+
+    }
+
+    @After
+    public void quit(){
         driver.close();
     }
 }
--- src/test/java/WishlistTest.java
+++ src/test/java/WishlistTest.java
@@ -1,16 +1,24 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WishlistTest {
+    private WebDriver driver;
 
+    @Before
+    public void initDriver(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new ChromeDriver();
 
-
-    public void addToWishlistTest(){
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://testfasttrackit.info/selenium-test/");
+    }
+
+    @Test
+    public void addToWishlistTest(){
 
         driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
         driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
@@ -19,8 +27,9 @@ public class WishlistTest {
         driver.findElement(By.id("send2")).click();
 
 
-
-
-
+    }
+    @After
+    public void quit(){
+        driver.close();
     }
 }
